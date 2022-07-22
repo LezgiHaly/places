b33b210af69e25e3c2a0061b6648b4b3847a032a
LezgiHaly <101892549+LezgiHaly@users.noreply.github.com>
2022-07-22 19:16:28 +0300

    homework4.2.2

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -11,7 +11,15 @@ class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        centerTitle: false,
+        toolbarHeight: 150,
+        title: appBarTitle(),
+      ),
       body: testWidget(),
     );
   }
@@ -20,5 +28,57 @@ class _SightListScreenState extends State<SightListScreen> {
 Widget testWidget() {
   return Center(
     child: Text("Hello!"),
+  );
+}
+
+Widget appBarTitle() {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      RichText(
+        text: TextSpan(
+          text: 'С',
+          style: TextStyle(
+            fontSize: 32,
+            color: Colors.green,
+            fontFamily: 'Roboto',
+            fontWeight: FontWeight.w700,
+          ),
+          children: [
+            TextSpan(
+              text: 'писок',
+              style: TextStyle(
+                fontSize: 32,
+                color: Colors.black,
+                fontFamily: 'Roboto',
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ],
+        ),
+      ),
+      RichText(
+        text: TextSpan(
+          text: 'и',
+          style: TextStyle(
+            fontSize: 32,
+            color: Colors.yellow,
+            fontFamily: 'Roboto',
+            fontWeight: FontWeight.w700,
+          ),
+          children: [
+            TextSpan(
+              text: 'нтересных мест',
+              style: TextStyle(
+                fontSize: 32,
+                color: Colors.black,
+                fontFamily: 'Roboto',
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ],
+        ),
+      )
+    ],
   );
 }
